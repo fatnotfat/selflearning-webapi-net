7424e30235790c4472ae110817b8f7a8286d8160
Fatnotfat <nguyenphat2711@gmail.com>
2023-05-06 00:44:57 +0700

    Mini project about api

---
+++ MyWebApiApp/Data/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebApiApp.Data
+{
+    [Table("Category")]
+    public class Category
+    {
+
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set;}
+    }
+}
