f56fa40370f57911c50a7650dd1453bd2e701b33
Fatnotfat <nguyenphat2711@gmail.com>
2023-05-06 01:49:02 +0700

    03_FluentAPI

---
+++ MyWebApiApp/MyWebApiApp/Controllers/CategoriesController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Data;
+using MyWebApiApp.Models;
+using System.Linq;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private MyDBContext _context;
+
+        public CategoriesController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var listCategories = _context.Categories.ToList();
+            return Ok(listCategories);
+        }
+
+        [HttpGet("{cateID}")]
+
+        public IActionResult GetByID(int cateID)
+        {
+            var cate = _context.Categories.SingleOrDefault(c => c.CategoryID == cateID);
+            if (cate != null) { return Ok(cate); }
+            else return NotFound();
+
+        }
+
+        [HttpPost]
+
+        public IActionResult AddNewCategory (CategoryModel categoryModel)
+        {
+            try
+            {
+                var category = new Category
+                {
+                    Name = categoryModel.Name
+                };
+                _context.Add(category);
+                _context.SaveChanges();
+                return Ok(category);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{cateID}")]
+
+        public IActionResult UpdateCategoryByID(int cateID, CategoryModel categoryModel)
+        {
+            var cate = _context.Categories.SingleOrDefault(c => c.CategoryID == cateID);
+            if (cate != null) 
+            {
+                cate.Name = categoryModel.Name;
+                _context.SaveChanges();
+                return NoContent(); 
+            }
+            else return NotFound();
+
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Migrations/20230505183711_AddOrderSchema.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyWebApiApp.Migrations
+{
+    public partial class AddOrderSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    ShipDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OrderStatus = table.Column<int>(type: "int", nullable: false),
+                    Consignee = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DeliveryAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    ProductID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ProductPrice = table.Column<double>(type: "float", nullable: false),
+                    Discount = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => new { x.OrderID, x.ProductID });
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order",
+                        column: x => x.OrderID,
+                        principalTable: "Order",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Product",
+                        column: x => x.ProductID,
+                        principalTable: "Product",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_ProductID",
+                table: "OrderDetail",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
